93c5d0c06582107caa8986fcf892c1f933e066dc
Otito Amuga <urphasin@gmail.com>
2026-08-15 01:15:30 -0600

    z

--- cplus_code/main.cpp
+++ cplus_code/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 int main() {
 
   std::vector<int> a = {1, 2, 3};
-  std::cout << a.capacity() << std::endl;
+  a.insert(a.begin(), 15);
+  a.push_back(2);
+  a.push_back(2);
+  a.push_back(2);
+  a.push_back(2);
+  a.push_back(2);
+  a.push_back(2);
+  a.push_back(12);
+  a.push_back(14);
+  std::cout << "a.capacity() = " << a.capacity() << std::endl;
+  std::cout << "a.size() = " << a.size() << std::endl;
   
 }
