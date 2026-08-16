6c878c05f934b90bcc219c521e92f14113b384d0
Otito Amuga <urphasin@gmail.com>
2026-08-16 10:51:22 -0600

    d

--- c_code/main.c
+++ c_code/main.c
@@ -4,7 +4,30 @@ int main() {
   printf("Hello, World!\n");
 
 
-  int a[3] = {1, 2, 3};
+  int arr[4] = { 1, 2, 3 };
+  int value = 0;
+
+  for (int i = 3; i > 0; --i) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  int a[3] = { 1, 2, 3 };
   for (int i = 0; i < 3; i++) {
     printf("a[%d] = %d\n", i, a[i]);
   }
