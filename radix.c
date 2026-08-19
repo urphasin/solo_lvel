b9ad4d3c392f0c85c0388e5f28681cf553fa5d44
Otito Amuga <urphasin@gmail.com>
2026-08-19 15:49:57 -0600

    j

--- radix.c
+++ radix.c
@@ -2,15 +2,16 @@
 #include <math.h>
 
 int main() {
-  int16_t x = 0b1000000010110101;
+  int16_t x = 0b0000000001010101;
 
-  printf("0b1000000010110101;\n");
+  printf("0b0000000001010101;\n");
   printf("x = %d\n", x);
-  printf("2^16 - 1 = %lg\n\n", (pow(2, 16) - 1));
 
-  x = ~x + 1;
-  printf("x = ~x + 1\n");
-  printf("x = -%d\n", x);
+  printf("~x + 1; = %d\n", ~x + 1);
+
+  printf("x << 3; = %d\n", x << 3);
+  printf("2^16 - 1 = %lg\n", (pow(2, 16) - 1));
+
 
   
   return 0;
