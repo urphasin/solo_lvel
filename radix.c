511f40c47e8c6f264a31b24614cb0e89427a1de5
Otito Amuga <urphasin@gmail.com>
2026-08-19 16:49:33 -0600

    c

--- radix.c
+++ radix.c
@@ -12,6 +12,7 @@ int main() {
   printf("x << 3; = %d\n", x << 3);
   printf("2^16 - 1 = %lg\n", (pow(2, 16) - 1));
 
+  printf("(x << 3) || (x >> (W_fixed_bit_width - 3)) = %d\n", (x << 3) || (x >> (16 - 3)));
 
   
   return 0;
