12b2879ea701bc769a82c6f8a2ddba59192bd935
Otito Amuga <urphasin@gmail.com>
2026-08-18 20:37:36 -0600

    s

--- absoluteValue.c
+++ absoluteValue.c
@@ -4,10 +4,10 @@
 
 int main() {
   printf("abs(-5) = %d\n", abs(-5));
-  printf("fabsf(-77.52) = %d\n", fabsf(-77.52f));
-  printf("fabs(-77.52) = %d\n", fabsf(-77.52));
+  printf("fabsf(-77.52) = %f\n", fabsf(-77.52f));
+  printf("fabs(-77.52) = %lf\n", fabs(-77.52));
   printf("labs(-20) = %ld\n", labs(-20L));
-  printf("llabs(-42000) = %lld\n", labs(-42000LL));
+  printf("llabs(-42000) = %lld\n", llabs(-42000LL));
 
   return 0;
 }
