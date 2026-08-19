e14f5081c6d07e914c3d12b2eaf47986b0dd1000
Otito Amuga <urphasin@gmail.com>
2026-08-19 01:04:49 -0600

    n

--- leftShifting.c
+++ leftShifting.c
@@ -4,8 +4,11 @@ int main() {
   u_int16_t x = 0b0000000010110101; // 181
 
   x = x << 1; // x * 2^k provided no overflow occurs
+  // 0b0000000101101010 = 362
+
 
   printf("x = %d\n", x);
+  printf("y = %d\n", 0b0000000101101010);
   
   return 0;
 }
