cb4b9b4d61f8efd6c3fe1a3d0398be68a48950d1
Otito Amuga <urphasin@gmail.com>
2026-08-19 20:37:29 -0600

    h

--- radix.c
+++ radix.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-  int16_t x = 0b0000000001010101;
+  u_int16_t x = 0b0000000001010101;
 
   printf("0b0000000001010101;\n");
   printf("x = %d\n", x);
@@ -10,9 +10,11 @@ int main() {
   printf("~x + 1; = %d\n", ~x + 1);
 
   printf("x << 3; = %d\n", x << 3);
-  printf("2^16 - 1 = %lg\n", (pow(2, 16) - 1));
+  printf("x >> 3; = %d\n", x >> 3);
 
-  printf("(x << 3) || (x >> (W_fixed_bit_width - 3)) = %d\n", (x << 3) || (x >> (16 - 3)));
+  printf("unsigned_bit 2^16 = %lg values\n", pow(2, 16));
+
+  printf("(x << 3) | (x >> (W_fixed_bit_width - 3)) = %d\n", (x >> 3) | (x << 13) );
 
   
   return 0;
