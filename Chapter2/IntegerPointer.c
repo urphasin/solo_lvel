2b5c1eef6b86e91f04e44dae155dfd2582a6a7c1
Otito Amuga <urphasin@gmail.com>
2026-08-22 12:30:24 -0600

    j

--- Chapter2/IntegerPointer.c
+++ Chapter2/IntegerPointer.c
@@ -3,9 +3,11 @@
 
 typedef int* IntegerPointer;
 int main() {
-  IntegerPointer A = malloc(sizeof(int));
+  IntegerPointer A = (char*)malloc(sizeof(int)); // optional to cast to IntegerPointer/int*
   *A = 50;
 
+  printf("A = %d\n", *A);
+  
   printf("sizeof(int) = %lu bytes\n", sizeof(int));
 
   printf("sizeof(IntegerPointer) = %lu bytes\n", sizeof(IntegerPointer));
