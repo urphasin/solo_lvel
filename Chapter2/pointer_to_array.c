80dfd013bfff6dc9fb3a6499781c25466216bfa2
Otito Amuga <urphasin@gmail.com>
2026-08-21 22:03:58 -0600

    j

--- Chapter2/pointer_to_array.c
+++ Chapter2/pointer_to_array.c
@@ -5,9 +5,9 @@
 int main() {
   int arr[] = {1, 2, 3, 4, 5};
   
-  int* ptr = arr;
+  int (*ptr)[5] = &arr;
 
   for(int i = 0; i < 5; i++) {
-    printf("arr[%d] = %d\n",i,  *(ptr + i));
+    printf("arr[%d] = %d\n",i,  *(*ptr + i));
   }
 }
