e8ac0e5c2d9e2e1b41b8796f299e4e464c87ddd5
Otito Amuga <urphasin@gmail.com>
2026-08-22 22:50:48 -0600

    d

--- Chapter1/precedence.c
+++ Chapter1/precedence.c
@@ -3,7 +3,13 @@
 int main() {
   int a = 1, b = 2, c = 3, d = 4, ans;
 
+  ans = a & b == c; 
+  printf("ans = a & b == c;  = %d\n", ans);
+
+  ans = (a & b) == c;
+  printf("ans = (a & b) == c; = %d\n", ans);
+
+  ans = a & (b == c);
+  printf("ans = a & (b == c); = %d\n", ans);
   
-  
-  printf("ans = %d\n", ans);
 }
