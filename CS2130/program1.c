e1c0e05e93d7872dfc399ec29cf5c1b14604f09b
Otito Amuga <urphasin@gmail.com>
2026-09-03 12:24:32 -0600

    j

--- CS2130/program1.c
+++ CS2130/program1.c
@@ -8,16 +8,16 @@
 
 const int SIZE = 100;
 
-void Union(int* A, int* B, size_t size) {
+void Union(int *A, int *B, size_t size) {
   int r[size];
   for (int i = 0; i < size; i++) {
-    
+
     r[i] = A[i];
 
-    for(int j = 0; j < size; j++) {
+    for (int j = 0; j < size; j++) {
 
-      for(int k = 0; k < size; k++) {
-        if(B[k] = r[j]) {
+      for (int k = 0; k < size; k++) {
+        if (B[k] = r[j]) {
           continue;
         }
 
@@ -28,14 +28,15 @@ void Union(int* A, int* B, size_t size) {
   }
 
 }
-void Intersection(int* A, int* B, size_t size) {}
-void SymmetricDifference(int* A, int* B, size_t size) {}
+void Intersection(int *A, int *B, size_t size) {}
+void SymmetricDifference(int *A, int *B, size_t size) {}
 
 int main() {
-  int set1[SIZE] = {1, 3, 5, 7, 9};
-  int set2[SIZE] = {1, 2, 4, 6, 7, 8, 10};
+  int set1[SIZE] = { 1, 3, 5, 7, 9 };
+  int set2[SIZE] = { 1, 2, 4, 6, 7, 8, 10 };
 
   Union(set1, set2, SIZE);
   Intersection(set1, set2, SIZE);
   SymmetricDifference(set1, set2, SIZE);
+
 }
