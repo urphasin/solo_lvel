a3970bc064c5ff034d0b0cdc05d53672620cd3a5
Otito Amuga <urphasin@gmail.com>
2026-09-03 11:00:09 -0600

    foosh

--- CS2130/program1.c
+++ CS2130/program1.c
@@ -15,4 +15,8 @@ void SymmetricDifference(int* A, int* B, size_t size) {}
 int main() {
   int set1[SIZE] = {1, 3, 5, 7, 9};
   int set2[SIZE] = {1, 2, 4, 6, 7, 8, 10};
+
+  Union(set1, set2, 20);
+  Intersection(set1, set2, 20);
+  SymmetricDifference(set1, set2, 20);
 }
