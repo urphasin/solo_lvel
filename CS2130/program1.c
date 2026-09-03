5a920e367cb289be5dbbd451949abcec46e1aa2b
Otito Amuga <urphasin@gmail.com>
2026-09-03 10:48:35 -0600

    push

--- CS2130/program1.c
+++ CS2130/program1.c
@@ -2,7 +2,19 @@
 #include <math.h>
 #include <stdlib.h>
 
+// A ∪ B
+// A ∩ B
+// A - B
+
+const int SIZE = 20;
+
+int Union() {}
+int Intersection() {}
+int SymmetricDifference() {}
+
 int main() {
-  double atoms = (180/2.2) * 0.7 * (1000/18) * pow(10, 24);
-  printf("atoms: %e\n", atoms);
+  int set1[SIZE] = {1, 3, 5, 7, 9};
+  int set2[SIZE] = {1, 2, 4, 6, 7, 8, 10};
+
+  
 }
