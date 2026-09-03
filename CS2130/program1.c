283bd6b360be8870764e43c4eabb4bcbae7a60a9
Otito Amuga <urphasin@gmail.com>
2026-09-03 12:23:29 -0600

    push

--- CS2130/program1.c
+++ CS2130/program1.c
@@ -6,9 +6,28 @@
 // A ∩ B
 // A - B
 
-const int SIZE = 20;
+const int SIZE = 100;
 
-void Union(int* A, int* B, size_t size) {}
+void Union(int* A, int* B, size_t size) {
+  int r[size];
+  for (int i = 0; i < size; i++) {
+    
+    r[i] = A[i];
+
+    for(int j = 0; j < size; j++) {
+
+      for(int k = 0; k < size; k++) {
+        if(B[k] = r[j]) {
+          continue;
+        }
+
+      }
+
+    }
+
+  }
+
+}
 void Intersection(int* A, int* B, size_t size) {}
 void SymmetricDifference(int* A, int* B, size_t size) {}
 
@@ -16,7 +35,7 @@ int main() {
   int set1[SIZE] = {1, 3, 5, 7, 9};
   int set2[SIZE] = {1, 2, 4, 6, 7, 8, 10};
 
-  Union(set1, set2, 20);
-  Intersection(set1, set2, 20);
-  SymmetricDifference(set1, set2, 20);
+  Union(set1, set2, SIZE);
+  Intersection(set1, set2, SIZE);
+  SymmetricDifference(set1, set2, SIZE);
 }
