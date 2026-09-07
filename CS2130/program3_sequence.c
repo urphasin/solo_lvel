2010265f9576d658159f7cb39d359bc5fa47f525
Otito Amuga <urphasin@gmail.com>
2026-09-07 00:40:18 -0600

    s

--- CS2130/program3_sequence.c
+++ CS2130/program3_sequence.c
@@ -4,6 +4,15 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+/*
+Generate the sequence aaaaabbbbbaaaabbbbaaabbbaabbab
+
+Special rules:
+1 - Do not use switch or if statements, and don't manipulate loops to make them match 
+    if statements either.
+2 - Do not use more than 4 for/do while/while loops in any combination.
+*/
+
 int main() {
-  
+
 }
