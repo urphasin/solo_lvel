de33d1696b5ad26dc1f0e747855fd4b4909b032c
Otito Amuga <urphasin@gmail.com>
2026-09-07 00:37:24 -0600

    foosh

--- CS2130/program2_hash.c
+++ CS2130/program2_hash.c
@@ -2,8 +2,22 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
+void hash(char sentence[]) {
+  int hash_aggregate = 0, hash_value = 0;
+  for (int i = 0; sentence[i] != '\0'; i++) {
+    // printf("ascii #: %c -> %d\n", sentence[i], (int)sentence[i]);
+
+    int alphabetNumber = (int)(tolower(sentence[i])) - 96;
+    // printf("alphabet: %c -> %d\n", sentence[i], alphabetNumber);
+    hash_aggregate += alphabetNumber;
+  }
+  hash_value = hash_aggregate % 31;
+  printf("HASH VALUE -> %d\n", hash_value);
+}
 
 int main() {
-  
+  char sentence[] = "abc";
+  hash(sentence);
 }
