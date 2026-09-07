0b53d5066ca804fe7f4d5339b7cb0c8623112f22
Otito Amuga <urphasin@gmail.com>
2026-09-06 21:54:15 -0600

    j

--- CS2130/program1.c
+++ CS2130/program1.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // A ∪ B
 // A ∩ B
 // A - B
 
-const int SIZE = 100;
+const int SIZE = 1000;
 
-void Union(int *A, int *B, size_t size) {
-  int r[size];
-  
+// utitlities
+bool contains(int target, const int arr[], int size) {
   for (int i = 0; i < size; i++) {
-    r[i] = A[i];
-  }
-
-  for (int j = 0; j < size; j++) {
-    
-    for (int k = 0; k < size; k++) {
-      if (r[k] == B[j]) {
-        continue;
-      }
+    if (target == arr[i]) {
+      return true;
     }
-
   }
+  return false;
 }
-void Intersection(int *A, int *B, size_t size) {}
-void SymmetricDifference(int *A, int *B, size_t size) {}
+
+void print_array(const int* arr, size_t size) {
+  for (int i = 0; i < size; i++) {
+    if (i != size - 1) {
+      printf("%d, ", arr[i]);
+    }
+    printf("%d", arr[i]);
+  }
+  printf("\n");
+}
+
+void set_union(const int *A, size_t sizeA, const int *B, size_t sizeB) {
+  int resultSize = 0;
+  int r[1000];
+  
+  for (int i = 0; i < sizeA; i++) {
+    if(!contains(A[i], r, resultSize)) {
+      r[resultSize] = A[i];
+    }
+  }
+
+  for (int i = 0; j < resultSize; j++) {
+    if(!contains(B[i], r, resultSize)) {
+      r[resultSize] = B[i];
+    } 
+  }
+
+  print_array(r, resultSize);
+}
+void set_intersection(int *A, int *B, size_t size) {}
+void set_symmetric_difference(int *A, int *B, size_t size) {}
 
 int main() {
   int set1[SIZE] = { 1, 3, 5, 7, 9 };
   int set2[SIZE] = { 1, 2, 4, 6, 7, 8, 10 };
 
-  Union(set1, set2, SIZE);
-  Intersection(set1, set2, SIZE);
-  SymmetricDifference(set1, set2, SIZE);
+  set_union(set1, SIZE, set2, SIZE);
+  // set_intersection(set1, set2, SIZE);
+  // set_symmetric_difference(set1, set2, SIZE);
 
 }
