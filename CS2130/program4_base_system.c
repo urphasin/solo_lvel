0c57f73a088029bf25271ebb6037899e626fee4a
Otito Amuga <urphasin@gmail.com>
2026-09-08 15:31:24 -0600

    fush

---
+++ CS2130/program4_base_system.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// 0b00001000; 
+
+void print_binary_16(u_int16_t val) {
+  int width = 16;
+
+  for (int i = width-1; i>=0; i--) {
+
+    u_int16_t bit = (val >> i) & 1;
+
+    printf("%d", bit);
+
+    if(i % 4 == 0) {
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
+int main() {
+
+  u_int16_t a = 21607;
+  print_binary_16(a);
+}
